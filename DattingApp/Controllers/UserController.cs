94824c2c20318586d4936ba406c83886537c9290
Ali Talebi <a.ali.talebi@gmail.com>
2021-12-21 21:31:12 +0330

    Add UserController

---
+++ DattingApp/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DattingApp.Data;
+using DattingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DattingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.User.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _context.User.Find(id);
+            return user;
+        }
+    }
+}
