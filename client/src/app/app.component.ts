0afea267ada9ba3b505e66f450d274239321ca0d
Ali Talebi <a.ali.talebi@gmail.com>
2021-12-26 17:47:26 +0330

    Add HttpGet Request to getting user

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'The Datting App';
+  user: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/user').subscribe(response => {
+      this.user = response;
+    }, error => {
+      console.log(error);
+    })
+  }
 }
