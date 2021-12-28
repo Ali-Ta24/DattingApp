2fdb9939e0f6891fcc261ccfa711df32d3dd6b6e
Ali Talebi <a.ali.talebi@gmail.com>
2021-12-28 19:17:20 +0330

    Fix

--- DattingApp/Startup.cs
+++ DattingApp/Startup.cs
@@ -56,7 +56,7 @@ namespace DattingApp
 
             app.UseRouting();
 
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
