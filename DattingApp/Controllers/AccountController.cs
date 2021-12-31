7d310cfac3f5ed822d2d9975b84dc122716d76a1
Ali Talebi <a.ali.talebi@gmail.com>
2021-12-31 21:09:46 +0330

    Add DTOs

--- DattingApp/Controllers/AccountController.cs
+++ DattingApp/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using DattingApp.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using DattingApp.DTOs;
 
 namespace DattingApp.Controllers
 {
@@ -20,19 +21,28 @@ namespace DattingApp.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
             using var hmac = new HMACSHA512();
-            var user = new  AppUser{
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
-            
+
             _context.User.Add(user);
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.User.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
