388d6441f28793d65cf2e71e6beceae63204616a
Ali Talebi <a.ali.talebi@gmail.com>
2022-01-02 15:14:28 +0330

    Create token logic

--- DattingApp/Service/TokenService.cs
+++ DattingApp/Service/TokenService.cs
@@ -1,17 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DattingApp.Entities;
 using DattingApp.Interface;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DattingApp.Service
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandeler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandeler.CreateToken(tokenDescriptor);
+
+            return tokenHandeler.WriteToken(token);
         }
     }
 }
